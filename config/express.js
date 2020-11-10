445466f8d57da04743023c59055b73dd3035cc4b
Tobin Weber <webertr@uw.edu>
2020-11-10 13:49:20 -0800

    Latest

--- config/express.js
+++ config/express.js
@@ -18,6 +18,9 @@ module.exports = function() {
     }));
     app.use(bodyParser.json());
     app.use(methodOverride());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
     
     require('../app/routes/index.server.routes.js')(app);
     
