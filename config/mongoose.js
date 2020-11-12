361e3ace6483a8b0299e0e394bf5c7f79c96ad86
Tobin Weber <webertr@uw.edu>
2020-11-12 12:11:23 -0800

    Working towards a logbook

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,11 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function() {
-    var db = mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+    var db = mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true,
+					   useFindAndModify: false });
+    
+    require('../app/models/user.server.model');
+    
     return db;
 };
