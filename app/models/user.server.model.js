78e5011c88c06e7ec0b07efc02b0e87024902881
Tobin Weber <webertr@uw.edu>
2020-11-12 12:46:25 -0800

    Iteration

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -6,13 +6,47 @@ var UserSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
+    username: {
+	type: String,
+	trim: true,
+	required: true,
+	unique: true
+    },
+    email: {
+	type: String,
+	index: true,
+	match: /.+\@.+\..+/
+    },
     password: String,
     created: {
 	type: Date,
 	default: Date.now
+    },
+    role: {
+	type: String,
+	enum: ['Admin', 'Owner', 'User']
+    },
+    password: {
+	type: String,
+	validate: [
+	    function(password) {
+		return password.length >= 6;
+	    },
+	    'Password should be longer'
+	]
     }
     
 });
+
+
+// Custom static methods
+UserSchema.statics.findOneByUsername = function (username, callback) {
+    this.findOne({ username: new RegExp(username, 'i') }, callback);
+};
+
+// Validate 
+UserSchema.methods.authenticate = function(password) {
+    return this.password === password;
+};
 
 mongoose.model('User', UserSchema);
--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function() {
-    
+
+    mongoose.set('useCreateIndex', true);
     var db = mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true,
 					   useFindAndModify: false });
     
