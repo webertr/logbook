e75cfb3616469d769bd6fa8962188fab41b188da
Tobin Weber <webertr@uw.edu>
2020-11-23 17:42:05 -0800

    Inching closer

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -9,5 +9,9 @@ module.exports = function(app) {
 	.get(articles.read)
 	.put(users.requiresLogin, articles.hasAuthorization, articles.update)
 	.delete(users.requiresLogin, articles.hasAuthorization, articles.delete);
+
+    // This will be called before any routes, and will only be called if paramteres defined
+    // on app routes. I also think this is only local to this file, which is called later
+    // as a require(file)(app)
     app.param('articleId', articles.articleByID);
 };
